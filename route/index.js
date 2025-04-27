12816ffb4228aac5b83b0d0e99d0afc7ed260045
Vanessa Buzzell <vanessabuzzell@live.com>
2025-04-26 23:01:56 -0400

    got rid of a space

--- route/index.js
+++ route/index.js
@@ -2,7 +2,6 @@ const routes = require('express').Router();
 const lesson1Controller = require('../controllers/lesson1');
 
 routes.get('/', lesson1Controller.vanessaRoute);
-
 routes.get('/joshua', lesson1Controller.joshuaRoute);
 routes.get('/harper', lesson1Controller.harperRoute);
 
