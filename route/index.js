e63f3d7d10322262616714328588c1af1ad0d490
Vanessa Buzzell <vanessabuzzell@live.com>
2025-05-04 01:29:41 -0400

    working on personal coding for week 2

--- route/index.js
+++
@@ -1,8 +0,0 @@
-const routes = require('express').Router();
-const lesson1Controller = require('../controllers/lesson1');
-
-routes.get('/', lesson1Controller.vanessaRoute);
-routes.get('/joshua', lesson1Controller.joshuaRoute);
-routes.get('/harper', lesson1Controller.harperRoute);
-
-module.exports = routes;
